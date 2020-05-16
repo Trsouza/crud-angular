1ce69c179791ff4cafc03006fc56c05ac41b072b
Tathiane <tathianesouza1@gmail.com>
2020-05-16 04:19:36 -0300

    Criação do header, nav e footer

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html' /* Dessa forma eu consigo referenciar esse arquivo */
+})
+export class AppComponent {
+  
+}
